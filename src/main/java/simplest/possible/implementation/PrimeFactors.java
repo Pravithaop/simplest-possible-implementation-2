9e8d943fef47e6fd701b937141cb86aecaf56929
Pravithaop <37397271+Pravithaop@users.noreply.github.com>
2021-01-04 16:24:37 +0530

    Update PrimeFactors.java

--- src/main/java/simplest/possible/implementation/PrimeFactors.java
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -1,19 +1,22 @@
 package simplest.possible.implementation;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+
 public class PrimeFactors {
-    public List  factors;
-    public <T> List<T> of(int i) {
-        if(i==1){
-            return factors;
-        }
-        else
-        {
+    List<Integer> factors = new ArrayList<Integer>();
+    public  List of(int i) {
+
+        if(i==1){return factors;}
+        else{
             factors.add(2);
             return factors;
-        }
 
+
+
+        }
 
 
     }
